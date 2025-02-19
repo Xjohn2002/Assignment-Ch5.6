a983dde86523b87fd982fe9e8881cba96a662c17
Xjohn2002 <xjohnson3@student.gsu.edu>
2025-02-19 12:20:56 -0500

    Added code for save Button to main class

--- app/src/main/java/com/example/myfinances/MainActivity.java
+++ app/src/main/java/com/example/myfinances/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.myfinances;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
@@ -67,5 +69,86 @@ public class MainActivity extends AppCompatActivity {
 
         });
     }
-    public void
+    public void initSaveBtn() {
+        Button saveButton = findViewById(R.id.saveBtn);
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                RadioButton cdRadioButton = findViewById(R.id.cdRadioBtn);
+                RadioButton loanRadioButton = findViewById(R.id.loanRadioBtn);
+                RadioButton checkingRadioButton = findViewById(R.id.checkingRadioBtn);
+
+                EditText accountNumber = findViewById(R.id.etAccountNum);
+                EditText initialBalance = findViewById(R.id.etInitialBalance);
+                EditText currentBalance = findViewById(R.id.etCurrentBalance);
+                EditText interestRate = findViewById(R.id.etInterestRate);
+                EditText paymentAmount = findViewById(R.id.etPaymentAmount);
+
+                int accountNum = 0;
+                double initialBal = 0;
+                double currentBal = 0;
+                double intRate = 0;
+                double paymentAmt = 0;
+
+                if (!accountNumber.getText().toString().isEmpty()) {
+                    accountNum = Integer.parseInt(accountNumber.getText().toString());
+                }
+                if (!initialBalance.getText().toString().isEmpty()) {
+                    initialBal = Integer.parseInt(initialBalance.getText().toString());
+                }
+                if (!currentBalance.getText().toString().isEmpty()) {
+                    currentBal = Integer.parseInt(currentBalance.getText().toString());
+                }
+                if (!interestRate.getText().toString().isEmpty()) {
+                    intRate = Integer.parseInt(interestRate.getText().toString());
+                }
+                if (!paymentAmount.getText().toString().isEmpty()) {
+                    paymentAmt = Integer.parseInt(paymentAmount.getText().toString());
+                }
+
+                if (cdRadioButton.isChecked()) {
+                    try {
+                        CD cd = new CD(accountNum, initialBal, currentBal, intRate);
+
+                        CdDb cdDb = new CdDb(MainActivity.this);
+
+                        cdDb.open();
+                        cdDb.insertVals(cd);
+                        cdDb.close();
+                        alert();
+                        clearText();
+                    } catch (Exception e) {
+                        System.out.println("Error with cd db");
+                    }
+                } else if (loanRadioButton.isChecked()) {
+                    try {
+                        Loan loan = new Loan(accountNum, initialBal, currentBal, paymentAmt, intRate);
+
+                        LoanDB loanDB = new LoanDB(MainActivity.this);
+
+                        loanDB.open();
+                        loanDB.insertVals(loan);
+                        loanDB.close();
+                        alert();
+                        clearText();
+                    } catch (Exception e) {
+                        System.out.println("Error with LoanDB");
+                    }
+                } else if (checkingRadioButton.isChecked()) {
+                    try {
+                        CheckingAccount checkingAccount =
+                                new CheckingAccount(accountNum, currentBal);
+
+                        CheckingDB checkingDB = new CheckingDB(MainActivity.this);
+                        CheckingDB.open();
+                        CheckingDB.insertVals(checkingAccount);
+                        alert();
+                        clearText();
+                    } catch (Exception e) {
+                        System.out.println("Error with CheckingDB");
+                    }
+                }
+            }
+        });
+    }
 }
