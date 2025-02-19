482193681fcc8b7118afa32a6f4abf1bb646617a
Xjohn2002 <xjohnson3@student.gsu.edu>
2025-02-19 15:37:27 -0500

    Added LoanDB class

--- app/src/main/java/com/example/myfinances/CdDb.java
+++ app/src/main/java/com/example/myfinances/CdDb.java
@@ -1,4 +1,34 @@
 package com.example.myfinances;
 
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
+
 public class CdDb {
+    private DBHelper dbHelper;
+    private SQLiteDatabase db;
+
+    public CdDb(Context context) {
+        /// Creates Db
+        dbHelper = new DBHelper(context);
+    }
+
+    public void open() throws SQLException {
+        db = dbHelper.getWritableDatabase();
+    }
+
+    public void close() {
+        db.close();
+    }
+
+    public void insertVals(CD cd) {
+        ContentValues vals = new ContentValues();
+        vals.put("accountNumber",cd.getAccountNumber());
+        vals.put("initialBalance",cd.getInitialBalance());
+        vals.put("currentBalance",cd.getCurrentBalance());
+        vals.put("interestRate",cd.getInterestRate());
+    }
+
 }
+
--- app/src/main/java/com/example/myfinances/LoanDB.java
+++ app/src/main/java/com/example/myfinances/LoanDB.java
@@ -1,4 +1,40 @@
 package com.example.myfinances;
 
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
+
 public class LoanDB {
+    private DBHelper dbHelper;
+    private SQLiteDatabase db;
+
+    public LoanDB(Context context) {
+        /// Creates Db
+        dbHelper = new DBHelper(context);
+    }
+
+    public void open() throws SQLException {
+        db = dbHelper.getWritableDatabase();
+    }
+
+    public void close() {
+        db.close();
+    }
+
+    public void insertVals(Loan loan) throws SQLException {
+        try{
+        ContentValues vals = new ContentValues();
+        vals.put("accountNumber",loan.getAccountNumber());
+        vals.put("initialBalance",loan.getInitialBalance());
+        vals.put("currentBalance",loan.getCurrentBalance());
+        vals.put("interestRate",loan.getInterestRate());
+        vals.put("paymentAmount",loan.getPaymentAmount());
+
+        this.db.insert("loan", null, vals);
+    } catch (Exception e) {
+        System.out.println("Error with loan db");
+    }
+
+}
 }
