83d7bafc52131c22d3f183987ec2eb83429fb2bf
Xjohn2002 <xjohnson3@student.gsu.edu>
2025-02-19 10:47:26 -0500

    Added code to enable/disable radio group btns when selecting account

--- app/src/main/java/com/example/myfinances/MainActivity.java
+++ app/src/main/java/com/example/myfinances/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.myfinances;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +23,49 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
     }
+    public void radioGroupChange(){
+        RadioGroup radioGroup = findViewById(R.id.rGroupAccountOptions);
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup radioGroup, int i) {
+                RadioButton cdRadioButton = findViewById(R.id.cdRadioBtn);
+                RadioButton loanRadioButton = findViewById(R.id.loanRadioBtn);
+                RadioButton checkingRadioButton = findViewById(R.id.checkingRadioBtn);
+
+                EditText accountNumber = findViewById(R.id.etAccountNum);
+                EditText initialBalance = findViewById(R.id.etInitialBalance);
+                EditText currentBalance = findViewById(R.id.etCurrentBalance);
+                EditText interestRate = findViewById(R.id.etInterestRate);
+                EditText paymentAmount = findViewById(R.id.etPaymentAmount);
+
+                if (cdRadioButton.isChecked()) {
+                    accountNumber.setEnabled(true);
+                    initialBalance.setEnabled(true);
+                    currentBalance.setEnabled(true);
+                    interestRate.setEnabled(true);
+                    paymentAmount.setEnabled(false);
+
+
+                } else if (loanRadioButton.isChecked()) {
+                    accountNumber.setEnabled(true);
+                    initialBalance.setEnabled(true);
+                    currentBalance.setEnabled(true);
+                    interestRate.setEnabled(true);
+                    paymentAmount.setEnabled(true);
+
+                } else if (checkingRadioButton.isChecked()) {
+                    accountNumber.setEnabled(true);
+                    initialBalance.setEnabled(false);
+                    currentBalance.setEnabled(true);
+                    interestRate.setEnabled(false);
+                    paymentAmount.setEnabled(false);
+                }
+
+            }
+
+        });
+    }
+    public void
 }
